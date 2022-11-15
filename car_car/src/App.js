d4f1a4cd3cbb583dd21eba3a4652951f1035f600
Mark <markcfleming1990@gmail.com>
2022-11-15 12:48:30 -0500

    working on tailwind

--- car_car/src/App.js
+++ car_car/src/App.js
@@ -1,20 +1,12 @@
-import React from 'react';
-import About from './Components/About/About.js';
-import Nav from './Components/Nav/Nav.js';
+// import React from 'react';
+// import About from './Components/About/About.js';
+import Nav from './Components/Nav/Nav.jsx';
 // import ContactForm from './Components/Contact/Contact.js';
-// import './App.css';
 
 function App() {
   return (
-    
-      <div>
-      <Nav></Nav>
-      
-      <main>
-  <About></About>
-  {/* <ContactForm></ContactForm> */}
-  </main>
-  </div>
+    <Nav />
+   
   );
 }
 
---
+++ car_car/src/Components/Nav/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Nav = () => {
+
+  return (
+  <div className='bg-cyan-200'>
+    <h1>Car Car Used Cars</h1>
+  </div>
+  );
+};
+
+
+export default Nav;
