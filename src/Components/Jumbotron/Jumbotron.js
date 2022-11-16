20fdd731534aee0afc42000f27af46e7704df7ec
Mark <markcfleming1990@gmail.com>
2022-11-16 13:37:04 -0500

    working on jumbotron

--- src/Components/Jumbotron/Jumbotron.js
+++ src/Components/Jumbotron/Jumbotron.js
@@ -3,7 +3,17 @@ import React from 'react'
 
  function Jumbotron() {
   return (
-    <div>Jumbotron</div>
+    <div className='flex'>
+    <div className='w-screen h-[500px] py-8 bg-slate-400'>
+       <h1 className='flex justify-center'>## VEHICLES AVAILABLE</h1>
+    
+    <div className='flex justify-items-center px-4'>
+    <button className='rounded-full bg-white text-4xl p-4'>Trade in</button>
+    <button className='rounded-full bg-white text-4xl'>View Specials</button>
+    <button className='rounded-full bg-white text-4xl'>View Inventory</button>
+    </div>
+    </div>
+    </div>
   )
 }
 
