6ba7402d6a5bfa45d8bb02ac4779048b848267e5
Mark <markcfleming1990@gmail.com>
2022-12-24 10:18:47 -0500

    working on background pictures for jumbo

--- src/Components/Jumbotron/Jumbotron.js
+++ src/Components/Jumbotron/Jumbotron.js
@@ -5,12 +5,12 @@ import React from 'react'
   return (
     <div className='grid grid-cols-3 gap-4 content-center'>
     <div className='w-screen h-[400px] p-8 bg-slate-400'>
-       <h1 className='flex justify-evenly text-3xl font-bold py-20'>## VEHICLES AVAILABLE</h1>
+       {/* <h1 className='flex justify-evenly text-3xl font-bold py-20'></h1> */}
     
     <div className='grid grid-cols-3 gap-8 px-4 py-16'>
-    <button className='rounded-full bg-white py-4 text-2xl'>Trade in</button>
-    <button className='rounded-full bg-white py-4 text-2xl'>View Specials</button>
-    <button className='rounded-full bg-white text-2xl'>View Inventory</button>
+    {/* <button className='rounded-full bg-white py-4 text-2xl'>About Us</button>
+    <button className='rounded-full bg-white py-4 text-2xl'>Information</button>
+    <button className='rounded-full bg-white text-2xl'>Our Partners</button> */}
     </div>
     </div>
     </div>
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -5,13 +5,15 @@ const Nav = () => {
   return (
     <div className='flex justify-between items-center h-[80px] px-4 bg-zinc-200 w-screen drop-shadow-lg'>
     <div>
-      <h1 className='text-3xl mr-4 font-bold'>Car Car Used Cars</h1>
+      <h1 className='text-3xl mr-4 font-bold'>Faith & Family Resources</h1>
     </div>
-    <ul className='flex'>
-      <li className='p-4 hover:bg-zinc-600'>About</li>
-      <li className='p-4 hover:bg-zinc-600'>Contact</li>
-      <li className='p-4 hover:bg-zinc-600'>Trade in</li>
-      <li className='p-4 hover:bg-zinc-600'>Inventory</li>
+    <ul className='flex p-4'>
+      <li className='p-4 hover:bg-zinc-600'>About Us</li>
+      <li className='p-4 hover:bg-zinc-600'>Contact Us</li>
+      <li className='p-4 hover:bg-zinc-600'>Information</li>
+      <li className='p-4 hover:bg-zinc-600'>Partners</li>
+      <li className='p-4 hover:bg-zinc-600'>Sign In</li>
+      <button className='rounded-full py-2 bg-slate-700 hover:bg-zinc-600'>Sign Up</button>
     </ul>
     </div>
   );
