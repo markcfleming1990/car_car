74464607f29d8ad8b10bc37974c6c46e03a4067d
Mark <markcfleming1990@gmail.com>
2022-12-26 18:48:04 -0500

    work on subscription elements

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,11 +5,15 @@ import customSolutions from '../../Assets/img/custom_soutions.png'
 
 function About() {
   return (
-    <div className="flex justify-center text-center w-auto h-[100px] py-10 bg-white">
+    <div className="flex flex-col text-center w-auto h-[400px] py-10 bg-white">
       
-      <div className="grid grid-cols-1 justify-center">
+      <div className="grid grid-cols-2 justify-center">
+        <div>
       <h1 className="text-2xl"> All in One Life Planning</h1>
-        <p className=''>
+      <h2>Lorem ipsum dolor sit amet, consectetur</h2>
+      </div>
+      <div>
+        <p className='text-left'>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
           bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus
           ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam
@@ -20,18 +24,19 @@ function About() {
           consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet
           ultricies mollis. In hac habitasse platea dictumst.
         </p>
+        </div>
       </div>
       <div className="grid grid-cols-3 gap-8 px-6 py-16">
         <div>
-        <img src={lifeplanning} alt='grid about us' />
+        <img src={lifeplanning} className='hover:opacity-25' alt='grid about us' />
           Life planning
         </div>
         <div>
-        <img src={networking} alt='grid about us' />
+        <img src={networking} className='hover:opacity-25' alt='grid about us' />
           Business Networking
         </div>
         <div>
-        <img src={customSolutions} alt='grid about us' />
+        <img src={customSolutions} className='hover:opacity-25' alt='grid about us' />
           Custom Solutions
         </div>
       </div>
--- src/Components/Values/Values.js
+++ src/Components/Values/Values.js
@@ -4,7 +4,7 @@ import React from 'react'
  function Values() {
   return (
     <div>
-      Values
+      
     </div>
 
   )
