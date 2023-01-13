16584bd2a2885320952e0522116d93348b868a4f
Mark <markcfleming1990@gmail.com>
2023-01-13 11:51:48 -0500

    added donate button to jumbotron

--- src/Components/Jumbotron/Jumbotron.js
+++ src/Components/Jumbotron/Jumbotron.js
@@ -7,11 +7,12 @@ import React from 'react';
     // add senior pictures
     <div className='grid grid-cols-3 gap-4 content-center'>
     <div className='w-screen h-400'>
-      <div className='grid cols-2'>
+      <div className='grid cols-2 gap-2'>
       <div className='text-2xl p-60 bg-yellow-600'>
         <h1>Faith & Family Resources</h1>
         <p>Bringing peace of mind to those that matter the most</p>
         <button className='rounded-full px-2 bg-blue-900'>Request Information</button>
+        <button className='rounded-full px-2 bg-blue-900'>Donate</button>
       </div>
       </div>
     {/* <img src={background} className='object-cover'alt="cover" /> */}
