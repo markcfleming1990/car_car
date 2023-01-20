6d464dc45cb8a447f99e22161331e0324542a86a
Mark <markcfleming1990@gmail.com>
2023-01-20 14:34:46 -0500

    added styling to donations

--- src/Components/Donations/Donations.js
+++ src/Components/Donations/Donations.js
@@ -3,15 +3,24 @@ import React from "react";
 
 function Donation() {
   return (
-    <div>
-      <h1>Donations</h1>
+    <div className='px-2 rounded border-2  bg-green-900'>
+      <h1 className='flex justify-center text-3xl'>Donations</h1>
       <div>
         <h2>Your Donations Matter!</h2>
       </div>
       <div>
-        <p>Lorem ipsum dolor sit amet, consectetur</p>
+      <div className='flex'>
+        <p>Donations help us empower our community
+          and give our seniors confidence in local business </p>
       </div>
-      <button>Learn More</button>
+      <div className='grid grid-cols-6 auto-cols-min gap-6 p-20'>
+        <h1 className='col-span-4'>Become a Donor</h1>
+      <button className='rounded border-2 border-black bg-yellow-800'>$10</button>
+      <button className='rounded border-2 border-black bg-yellow-800'>$20</button>
+      <button className='rounded border-2 border-black bg-yellow-800'> $30</button>
+      <button className='rounded border-2 border-black bg-yellow-800'>Other</button>
+      </div>
+      </div>
     </div>
   )
 }
