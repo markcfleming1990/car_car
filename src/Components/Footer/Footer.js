bd8b31f3e568586dab095217684fc6797d9b5e30
markcfleming <markcfleming1990@gmail.com>
2023-02-08 16:07:29 -0500

    working on footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,21 +2,29 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="flex bg-green  ">
-      <footer>
-        <h1 className="text-gold text">Contact Us</h1>
-        <form>
-          <input type="text" placeholder="Name" />
-          <input type="email" placeholder="Email" />
-          <input type="text" placeholder="Subject" />
-          <button className="rounded-full px-2 bg-gold text-black">
-            Submit
-          </button>
-        </form>
-        <section>
+    <div>
+      <footer className="grid grid-cols-3 content-start bg-green p-[60px]">
+        <div>
+          <h1 className="text-gold text-center">Contact Us</h1>
+          <form className="grid justify-items-center grid-cols-2">
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email" />
+            <input type="text" className="" placeholder="Subject" />
+            <button className="rounded-full px-2 bg-gold text-black text-center align-center">
+              Submit
+            </button>
+          </form>
+        </div>
+        <div className="flex text-center">
+          <h1 className="text-2xl text-gold">Menu</h1>
+          <button>About Us</button>
+          <button>Partners</button>
+          <button>Donations</button>
+        </div>
+        <div>
           <h1>Senior Resource Network</h1>
           <p>1657 Commerce Dr Suite 7B3 South Bend, IN 46628 877-324-8490</p>
-        </section>
+        </div>
       </footer>
     </div>
   );
