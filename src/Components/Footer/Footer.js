3a8d8c2200ce8a4bd913d47f19af5120be2c81e7
markcfleming <markcfleming1990@gmail.com>
2023-02-09 08:13:47 -0500

    got addres and menu in footer styled

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,25 +5,41 @@ function Footer() {
     <div>
       <footer className="grid grid-cols-3 content-start bg-green p-[60px]">
         <div>
-          <h1 className="text-gold text-center">Contact Us</h1>
-          <form className="grid justify-items-center grid-cols-2">
+          <h1 className="text-gold text-center text-2xl">Contact Us</h1>
+          <form className="">
             <input type="text" placeholder="Name" />
             <input type="email" placeholder="Email" />
-            <input type="text" className="" placeholder="Subject" />
-            <button className="rounded-full px-2 bg-gold text-black text-center align-center">
-              Submit
-            </button>
+            <input type="text" placeholder="Subject" />
+            <button className="rounded-full">Submit</button>
           </form>
         </div>
-        <div className="flex text-center">
+
+        <div className="text-center">
           <h1 className="text-2xl text-gold">Menu</h1>
-          <button>About Us</button>
-          <button>Partners</button>
-          <button>Donations</button>
+          <div className="text-gold grid grid-rows-4 gap-2">
+            <div>
+              <button>Home</button>
+            </div>
+            <div>
+              <button>About Us</button>
+            </div>
+            <div>
+              <button>Partners</button>
+            </div>
+            <div>
+              <button>Donations</button>
+            </div>
+          </div>
         </div>
         <div>
-          <h1>Senior Resource Network</h1>
-          <p>1657 Commerce Dr Suite 7B3 South Bend, IN 46628 877-324-8490</p>
+          <h1 className="text-gold text-center text-2xl">
+            Senior Resource Network
+          </h1>
+          <div className="text gold text-center">
+            <p>1657 Commerce Dr Suite 7B3</p>
+            <p>South Bend, IN 46628</p>
+            <p>877-324-8490</p>
+          </div>
         </div>
       </footer>
     </div>
