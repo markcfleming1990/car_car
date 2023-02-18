c32fa8cdf1e7fd43ac7a3491daf223a3e97fb76d
markcfleming <markcfleming1990@gmail.com>
2023-02-18 09:06:06 -0500

    working teams page structure

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Maria from "../Assets/img/Maria_Team.png";
 
 const Team = () => {
   return (
     <div>
-      <h1>Team</h1>
+      <p>Our team is dedicated to bringing the best to our senior community</p>
+
+      <div>
+        <img src={Maria} alt="Maria Tanksley CEO and Founder">
+          <p>Maria Tanksley</p>
+          <p>CEO/Founder</p>
+          <p>email@email.com</p>
+        </img>
+      </div>
     </div>
   );
 };
