5abf2df937623feca4cc6797eb10d30d4edd5dcb
markcfleming <markcfleming1990@gmail.com>
2023-03-08 13:05:26 -0500

    deployment changes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("server Listening"));
+app.listen(process.env.PORT || 5000, () => console.log("server Listening"));
 
 const contactEmail = nodemailer.createTransport({
   service: "gmail",
