c52cf0d577f06a664af51f661c026166e976096d
markcfleming <markcfleming1990@gmail.com>
2023-03-08 14:02:14 -0500

    working tailwind issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-// /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   important: true,
   content: ["/public/*.html", "./src/components/**/*.{jsx,js}"],
