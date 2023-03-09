ea259aa786d51fc4535688e9e641ece3aa6228d2
markcfleming <markcfleming1990@gmail.com>
2023-03-09 10:25:18 -0500

    components not styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,9 +26,5 @@ module.exports = {
     },
     extend: {},
   },
-  plugins: [
-    require("@tailwindcss/forms"),
-    require("tailwindcss"),
-    require("autoprefixer"),
-  ],
+  plugins: [require("tailwindcss"), require("autoprefixer")],
 };
