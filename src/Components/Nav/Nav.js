22a3e900695b6155cd2e6a891721c1c8090ec496
markcfleming <markcfleming1990@gmail.com>
2023-03-09 10:30:54 -0500

    working on inspect error

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -21,7 +21,7 @@ const Nav = () => {
               <NavLink
                 to={link.path}
                 className="hover:text-blue-100 hover:bg-gold rounded py-2 px-4 text-lg font-medium tracking-wider"
-                activeClassName="text-blue-100 bg-gold"
+                // className="text-blue-100 bg-gold"
               >
                 {link.label}
               </NavLink>
