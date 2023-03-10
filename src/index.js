e97de311c591dc193b2330fb3285baa7c262e7bc
markcfleming <markcfleming1990@gmail.com>
2023-03-10 15:27:56 -0500

    working deployment issue

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import "./tailwind.css";
 import App from "./App";
-// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
