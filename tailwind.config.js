ec5c7c71148b40f736c49f1fb4074ed156624c50
markcfleming <markcfleming1990@gmail.com>
2023-03-12 10:14:41 -0400

    working deployment issue with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,56 +1,31 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   important: true,
-  content: [
-    "/public/*.html",
-    "./src/components/**/*.{jsx,js}",
-    "./src/pages/**/*.{jsx,js}",
-    "./src/App.js.{jsx,js}",
-  ],
+  content: ["./src/components/**/*.{jsx,js}", "./src/pages/**/*.{jsx,js}"],
   theme: {
-    extend: {
-      colors: {
-        blue: "#0C2340",
-        gold: "#C99700",
-        green: "#00843D",
-      },
-      screens: {
-        sm: "640px",
-        md: "768px",
-        lg: "1024px",
-        xl: "1280px",
-        "2xl": "1536px",
-      },
-      // Add support for the appearance property
-      appearance: ["responsive"],
-
-      // Add support for the -webkit-font-smoothing property
-      webkitFontSmoothing: ["responsive", "hover"],
-      mozOsxFontSmoothing: ["responsive", "hover"],
+    colors: {
+      blue: "#0C2340",
+      gold: "#C99700",
+      green: "#00843D",
     },
-  },
-  variants: {
-    // Add variants for the appearance and -webkit-font-smoothing properties
-    appearance: ["responsive"],
-    webkitFontSmoothing: ["responsive", "hover"],
-    mozOsxFontSmoothing: ["responsive", "hover"],
-  },
-  plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
 
-    // Generate the new utility classes for -webkit-font-smoothing
-    function ({ addUtilities }) {
-      const newUtilities = {
-        ".antialiased": {
-          webkitFontSmoothing: "antialiased",
-          mozOsxFontSmoothing: "grayscale",
-        },
-        ".subpixel-antialiased": {
-          webkitFontSmoothing: "subpixel-antialiased",
-          mozOsxFontSmoothing: "auto",
-        },
-      };
-      addUtilities(newUtilities, ["responsive", "hover"]);
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
     },
-  ],
+    extend: {},
+  },
+  variants: "all",
+  plugins: [require("tailwindcss"), require("autoprefixer")],
 };
