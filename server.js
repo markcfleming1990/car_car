7752995ed15977afb5455e5f398440cd47eb6ca9
markcfleming <markcfleming1990@gmail.com>
2023-03-14 11:47:54 -0400

    reverting some changes

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const path = require("path");
 const express = require("express");
+const router = express.Router();
+const cors = require("cors");
 const app = express();
-const publicPath = path.join(__dirname, ".", "public");
-const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
-});
-app.listen(port, () => {
-  console.log("Server is up!");
-});
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+app.listen(process.env.PORT || 5000, () => console.log("server Listening"));
